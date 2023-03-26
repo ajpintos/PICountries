e710d958c07a93e2d1f32b9a47ee8e78b3209da2
ajpintos <ajpintos@gmail.com>
2023-03-25 22:52:18 -0300

    Terminado el Backend y modularizado!!!

--- api/src/handlers/countriesHandler.js
+++ api/src/handlers/countriesHandler.js
@@ -1,31 +1,14 @@
 const {DataTypes} = require("sequelize");
 const {Country, CountriesInDb} = require("../db");
 const axios = require('axios');
-const {createCountries} = require("../controllers/countriesController");
+const {createCountries, cleanArray} = require("../controllers/countriesController");
 const {createCountriesInDb, createActivity} = require("../controllers/activitiesController");
 
-
 const getCountriesHandler = async (req, res) => {
-    const { name, id } = req.query;
+    const {name, id} = req.query;
     let results = name ? await searchCountryByName(name) : id ? await getCountriesByIdHandler(id) : await ifTableCountriesIsEmpty();
     res.status(200).send(results);
 };
-
-//! esta funcion limpia los datos de la API
-const cleanArray = (arr) =>
-    arr.map(elem => {
-        return {
-            id: elem.cca3,
-            name: elem.name ? elem.name[Object.keys(elem.name)[1]] : "No Data Available",
-            flag: elem.flag ? elem.flags[1] : "No Data Available",
-            continent: elem.continent ? elem.continent : "No Data Available",
-            capital: elem.capital ? elem.capital[0] : "No Data Available",
-            subregion: elem.subregion ? elem.subregion : "No Data Available",
-            area: elem.area ? elem.area : null,
-            population: elem.population ? elem.population : 0,
-        };
-    });
-
 
 //! Esta función verifica si la tabla Countries esta vacia, si esta vacia hace el llamado a la API, si no esta vacia muestra los datos de la DB
 async function ifTableCountriesIsEmpty() {
@@ -35,28 +18,6 @@ async function ifTableCountriesIsEmpty() {
     } else {
         return await showCountriesFromDb();
     }
-}
-
-
-//! Función que trae todos los usuarios de la API
-const getAllCountriesFromApi = async () => {
-    const databaseUsers = await Country.findAll();
-    const apiUserRaw = (
-        await axios.get("https://restcountries.com/v3/all")
-    ).data;
-    const apiCountries = await cleanArray(apiUserRaw);
-    return apiCountries;
-}
-
-//! Función que guarda los datos de la API en la DB
-const saveInDb = async () => {
-    try {
-        const results = await Country.bulkCreate(await getAllCountriesFromApi());
-        console.log('Countries saved from api:', results.length);
-    } catch (error) {
-        console.error('Error creating users', error);
-    }
-    ;
 }
 
 //? **** Función que trae todos los countries guardados en la DB **** //
